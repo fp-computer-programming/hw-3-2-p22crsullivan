6827dbc8641d3a867b72c682949798371fa90f65
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-09-30 11:20:16 -0400

    Submit HW 3-2-2

---
+++ hw3-2-2.py
@@ -0,0 +1,14 @@
+# Author: CRS 9/29/21
+weight = int(input("Please enter your weight."))
+height = int(input("Please enter your height."))
+bmi = weight / height
+if bmi < 19:
+    print("Underweight.")
+elif bmi < 25:
+    print("Healthy.")
+elif bmi < 30:
+    print("Overweight.")
+elif bmi < 40:
+    print("Obese.")
+elif bmi >= 40:
+    print("Extremely obese.")
